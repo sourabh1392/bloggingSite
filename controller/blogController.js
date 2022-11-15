4c38746de398b1126eba10c848011ac7f3a68041
garimaa0907 <sachangarima07@gmail.com>
2022-11-15 21:29:00 +0530

    apis did

--- controller/blogController.js
+++ controller/blogController.js
@@ -10,36 +10,50 @@ const createBlog= async function(req,res){
     const create=await blogModel.create(data)
     res.status(201).send({status:true,message:"Blog Created",data:create})
     }
-    catch(err){
+    catch(err) {
     res.status(500).send(err.message)
     }
 }
 
-const getBlog=async function(req,res){
+
+const putblogs= async function(req,res){
     try{
-        const blogdata=await blogModel.find({isDeleted:false},{isPublished:true}).populate("authorId")
-        if(!blogdata) return res.status(404).send("No Such Blog Found")
-        res.status(200).send({status:true,message:"BlogList",data:blogdata })
+     let {title ,body , subcategory, tags} = req.body 
+     if(!title && !body && !subcategory && !tags) 
+     return res.status(400).send("the documents needs to be updated is not present")
+     let blogdata= await blogModel.findByIdAndUpdate({_id: blogId}, {$set:title},{new : true})
+     res.status(200).send({msg : "the updated documents", data:blogdata})
     }
     catch(err){
         res.status(500).send(err.message)
-    }
-
-}
-const getPerticularBlog=async function(req,res){
+    }}
+ 
+const deleteblog = async function (req,res){
     try{
-        const data=req.query
-        const getdata=await blogModel.find({$or:[{authorId:data.authorId},{category:data.category},{tags:data.tags},{subcategory:data.subcategory }]}).populate("authorId")
-        if(!getdata) return res.status(400).send("No such blog found")
-        res.status(200).send({status:true,message:"Filtered Blog",data:getdata})
+         const blogId= req.blogId
+         let deleteData = await blogModel.findOneAndUpdate({_id:blogId},{isDeleted: true},{new:true})
+         res.status(200).send({msg :"updated", data:deleteData})
     }
     catch(err){
         res.status(500).send(err.message)
+    }}
+
+const deleteQueryBlog = async function (req,res)
+{try{
+     
+    const {category , authorId , tags , subcategory , isPublished }= req.query
+    if(!category && !authorId && !tags && !subcategory && !isPublished){
+    return res.status(404).send({status:false, message:"no documents for deletion"})
     }
-
+    let deleteda= await blogModel.findOneAndUpdate({category: category},{isDeleted: true},{new:true})
+    res.status(200).send({msg :"updated", data:deleteda})
 }
-
-
+catch(err)
+{
+    res.status(500).send(err.message)
+}}
 module.exports.createBlog=createBlog
 module.exports.getBlog=getBlog
-module.exports.getPerticularBlog=getPerticularBlog
+module.exports.putblogs= putblogs
+module.exports.deleteblog = deleteblog
+module.exports.deleteQueryBlog= deleteQueryBlog
--- route/route.js
+++ route/route.js
@@ -2,10 +2,15 @@ const express=require("express")
 const router=express.Router()
 const author=require("../controller/authorcontroller")
 const blog=require("../controller/blogController")
+const calmidd = require("../middlewares/middle")
 
 router.post("/authors",author.createAuthor)
 router.post("/blogs",blog.createBlog)
-router.get("/blogs",blog.getBlog)
-router.get("/getPerticularBlog",blog.getPerticularBlog)
+router.get("/getblogs",blog.getBlog)
+
+router.put("/blogs/:blogId" ,calmidd.blogIDexist, blog.putblogs)
+router.delete("/blogs/:blogId" ,calmidd.blogIDexist, blog.deleteblog)
+
+router.delete("/blogs" ,blog.deleteQueryBlog)
 
 module.exports=router
